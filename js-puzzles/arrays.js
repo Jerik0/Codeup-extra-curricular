402cae7559de1d76caf32d34803b2d0c06939fe0
Jeriko <jcarrico1985@live.com>
2018-03-26 10:42:18 -0500

    added arrays.js, empty files: objects.js and strings.js. added appropriate script tags for index-for-practice.html.

---
+++ js-puzzles/arrays.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var names = ['bob', 'george', 'harry', 'jonathan', 'emily'];
+
+//TODO write a function that will console log each name using a for loop.
+
+//TODO write the same function refactored to use a forEach loop.
+
+//TODO write a function that will console log only the index of each element in the array.
+
+//TODO write a function that will capitalize each word in the array.
